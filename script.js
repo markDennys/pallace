772133648d233c25b95038b1a7c75b82ef4e07d2
Vilela <markus.dennys.vilela@accenture.com>
2024-05-27 22:30:34 -0300

    ad

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ const mtObserve = new IntersectionObserver((entr) => {
   entr.forEach((ent) => {
     if (ent.isIntersecting) {
       ent.target.classList.add("show");
-    }  
+    }
   });
 });
 
 const elems = document.querySelectorAll(".parag");
 
 elems.forEach((element) => mtObserve.observe(element));
+
+const mtObserve2 = new IntersectionObserver((entr) => {
+  entr.forEach((ent) => {
+    if (ent.isIntersecting) {
+      ent.target.classList.add("show");
+    } else {
+      ent.target.classList.remove("show");
+    }
+  });
+});
+
+const elems2 = document.querySelectorAll(".ani");
+const elems3 = document.querySelectorAll(".anit");
+const elems4 = document.querySelectorAll(".anim");
+const elems5 = document.querySelectorAll(".avec");
+
+elems2.forEach((element) => mtObserve2.observe(element));
+
+elems3.forEach((element) => mtObserve2.observe(element));
+
+elems4.forEach((element) => mtObserve2.observe(element));
+
