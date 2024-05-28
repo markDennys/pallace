197e0150b2f9a6886700684b001fb099e6eeab99
Vilela <markus.dennys.vilela@accenture.com>
2024-05-28 01:43:28 -0300

    pf

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ elems3.forEach((element) => mtObserve2.observe(element));
 
 elems4.forEach((element) => mtObserve2.observe(element));
 
+
+
+ 
+
+function enviarMensagemWhatsApp() {
+  // Número de telefone para o qual você deseja enviar a mensagem
+  var numeroTelefone = "558194622071"; // Substitua pelo número desejado, incluindo o código do país
+
+  // Mensagem pré-selecionada
+  var mensagem = "Olá! Gostaria de fazer um orçamento para um dia de noivo Pallace.";
+
+  // Codifica a mensagem para que possa ser incluída no URL
+  var mensagemCodificada = encodeURIComponent(mensagem);
+
+  // Monta o link para o WhatsApp com o número de telefone e a mensagem pré-selecionada
+  var linkWhatsApp = "https://wa.me/" + numeroTelefone + "?text=" + mensagemCodificada;
+
+  // Redireciona para o WhatsApp
+  window.location.href = linkWhatsApp;
+}
